da71247ddf6ae161040f1f12ade6a4957403c7a3
Andrew Jefferson Mendez <andrewjeffmendez30@gmail.com>
2026-02-16 18:18:33 +0800

    chore: add prettier and eslint configs

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+export default {
+	tailwindStylesheet: './src/app/globals.css',
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 110,
+	endOfLine: 'auto',
+	htmlWhitespaceSensitivity: 'ignore',
+	useTabs: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
